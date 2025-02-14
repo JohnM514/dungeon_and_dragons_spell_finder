bb2e2d7b5d904bd4bced7acfbca0f99fe5e3d22b
JohnM514 <juandedios.carranzam@gmail.com>
2025-02-13 21:23:47 -0600

    Add dependencies

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-import express from 'express'
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.listen(port, ()=>{
-    console.log('Server running on port ' + port)
-})
+app.set(express.static("public"));
+
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
